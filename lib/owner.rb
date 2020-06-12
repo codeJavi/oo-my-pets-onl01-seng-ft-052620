f4809d4d0595f251accf82db3513e94ddf522ee0
Javi Leyva <jvrleyva@gmail.com>
2020-06-12 21:31:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,63 +1,66 @@
 class Owner
-  OWNERS = []
-  attr_accessor :name, :pets
-  attr_reader :species
+  # code goes here
+  attr_reader :name, :species
 
-  def self.reset_all
-    OWNERS.clear
-  end
+  @@all = []
 
-  def self.all
-    OWNERS
-  end
-
-  def self.count
-    OWNERS.size
-  end
-
-  def initialize(species)
-    @species = species
-    OWNERS << self
-    @pets = {:dogs => [], :cats => []}
-  end
-  
-  def buy_dog(name)
-    pets[:dogs] << Dog.new(name)
-  end
-
-  def buy_cat(name)
-    pets[:cats] << Cat.new(name)
-  end
-
-  def walk_dogs
-    pets[:dogs].each do |dog|
-      dog.mood = "happy"
-    end
-  end
-
-  def play_with_cats
-    pets[:cats].each do |cat|
-      cat.mood = "happy"
-    end
-  end
-  
-  def sell_pets
-    pets.each do |species, animals|
-      animals.each do |animal|
-        animal.mood = "nervous"
-      end
-      animals.clear
-    end
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
   end
 
   def say_species
-    "I am a #{species}."
+    "I am a #{self.species}."
   end
 
-def list_pets
-    "I have #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  def self.all
+    @@all
   end
 
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def cats
+    Cat.all.select{|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each { |dog| dog.mood = "happy" }
+  end
+
+  def feed_cats
+    self.cats.each { |cat| cat.mood = "happy" }
+  end
+
+  def sell_pets
+    pets = self.dogs + self.cats
+
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
-
     
